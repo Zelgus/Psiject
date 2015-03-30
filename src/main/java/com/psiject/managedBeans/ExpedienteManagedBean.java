9226fff23cbc08ffe8633d90b05e5e860cdd6aa3
Zelgus <ivanom90@hotmail.com>
2015-03-30 18:04:37 +0200

    Bug generar usuario

--- src/main/java/com/psiject/managedBeans/ExpedienteManagedBean.java
+++ src/main/java/com/psiject/managedBeans/ExpedienteManagedBean.java
@@ -412,21 +412,25 @@ public class ExpedienteManagedBean implements Serializable {
 	}
 
 	public void generarUsuario() {
-		Character caracteres[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
-				'9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-				'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-				'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-				'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-				'v', 'w', 'x', 'y', 'z' };
-		String pass = "";
-		for (int i = 0; i < 6; i++) {
-			int caracter = new Random().nextInt(62);
-			pass += caracteres[caracter];
-		}
+		if (this.nombre.length() < 3 || this.apellidos.length() < 3) {
+			Mensajes.mostrarMensajeAlerta("Debe escribir un nombre y un apellido valido");
+		} else {
+			Character caracteres[] = { '0', '1', '2', '3', '4', '5', '6', '7',
+					'8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+					'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+					'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+					'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+					'u', 'v', 'w', 'x', 'y', 'z' };
+			String pass = "";
+			for (int i = 0; i < 6; i++) {
+				int caracter = new Random().nextInt(62);
+				pass += caracteres[caracter];
+			}
 
-		this.usuario = (this.nombre.substring(0, 3).concat(this.apellidos
-				.substring(0, 3)));
-		this.contrasena = (pass);
+			this.usuario = (this.nombre.substring(0, 3).concat(this.apellidos
+					.substring(0, 3)));
+			this.contrasena = (pass);
+		}
 	}
 
 	/*
